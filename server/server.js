63b61f0e6111d94fded91c159992a2a30c3621d1
Spencer <sp797499@dal.ca>
2025-01-15 12:55:28 -0400

    added google api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 
 const app = express();
 const PORT = 5000;
+
+app.get("/books", async (req, res) => {
+    const prompt = req.body;
+})
 
 
 app.listen(PORT, () => {
