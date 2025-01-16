d377db16d2f80bc0e1e0c4082018f10ac181b7ae
Spencer <sp797499@dal.ca>
2025-01-16 15:46:37 -0400

    diplaying data from promise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,29 @@ import './App.css'
 
 function App() {
 
+  const [suggestions, setSuggestions] = useState();
+
   async function serverFetch() {
 
     const resp = await fetch("http://localhost:5000/books")
 
-    console.log(resp.json())
+    const data = resp.json()
+
+    data.then(resp => {
+      setSuggestions(JSON.stringify(resp))
+    })
+
+    
+
     
   }
 
   return (
     <>
       <button onClick={serverFetch}></button>
+      <div>
+        {suggestions}
+      </div>
     </>
   )
 }
