74ba8568148456766df4d1325dcae3219d04739a
Spencer <sp797499@dal.ca>
2025-01-21 11:41:13 -0400

    added buttons for user prompts

--- client/src/Params.jsx
+++ client/src/Params.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react"
 
 
 
@@ -5,26 +6,57 @@
 
 function Params(props) {
 
-    const setSuggestions = props.handleSearch
+    const [genres, setGenres] = useState(["test"]);
+    const [books, setBooks] = useState([]);
+    const genreRef = useRef()
+    const booksRef = useRef()
 
-    console.log(setSuggestions)
+    
+
+    const addGenre = (e) => {
+
+        e.preventDefault()
+
+        console.log("button")
+        console.log(genres)
+        setGenres(prev => {return [...prev, genreRef.current.value]})
+        
+        //genreRef.current.value = ""
+    }
+
+    useEffect(() => {
+        genreRef.current.value = ""
+    }, [genres])
+
+
+    const setSuggestions = props.handleSearch;
+
 
     return (
         <>
         
         <div id="params">
-            <form className="param-form" action="/" method="post">
-                <label htmlFor="genre">which book genres you would like to see?:<br/></label>
-                <div>
-                    <input type="text" name="genre" className="param-input" />
-                    <button className="prompt-button">Add Genre</button>
+            <div>
+                <div className="user-prompts">
+                    {genres.map((genre, index) => {
+                        return (
+                            <div className="user-prompt" key={index}>{genre}</div>
+                        )
+                    })}
                 </div>
-            </form>
+                <form onSubmit={addGenre} className="param-form" method="post">
+                    <label htmlFor="genre">which book genres you would like to see?:<br/></label>
+                    <div>
+                        <input ref={genreRef} type="text" name="genre" className="param-input" />
+                        <button className="prompt-button">Add Genre</button>
+                    </div>
+                </form>
+            </div>
             <form className="param-form" action="/" method="post">
                 <label htmlFor="books">list books in a style you would like to read more of?:</label>
                 <div >
-                    <input type="text" name="books" className="param-input" />
-                    <button className="prompt-button">Add Book</button>
+                    <input ref={booksRef} type="text" name="books" className="param-input" />
+                    <button  className="prompt-button">Add Book</button>
                 </div>
             </form>
         
