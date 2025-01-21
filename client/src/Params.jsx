d145eb6ecbf52841f47a94583cd4739cc73e3ec9
Spencer <sp797499@dal.ca>
2025-01-20 23:06:59 -0400

    added buttons

--- client/src/Params.jsx
+++ client/src/Params.jsx
@@ -14,12 +14,18 @@ function Params(props) {
         
         <div id="params">
             <form className="param-form" action="/" method="post">
-                <label htmlFor="genre">Select which book genres you would like to see?:<br/></label>
-                <input type="text" name="genre" className="param-input" />
+                <label htmlFor="genre">which book genres you would like to see?:<br/></label>
+                <div>
+                    <input type="text" name="genre" className="param-input" />
+                    <button className="prompt-button">Add Genre</button>
+                </div>
             </form>
             <form className="param-form" action="/" method="post">
-                <label htmlFor="books">What are some books in a style you would like to read more of?:</label>
-                <input type="text" name="books" className="param-input" />
+                <label htmlFor="books">list books in a style you would like to read more of?:</label>
+                <div >
+                    <input type="text" name="books" className="param-input" />
+                    <button className="prompt-button">Add Book</button>
+                </div>
             </form>
         
         </div>
