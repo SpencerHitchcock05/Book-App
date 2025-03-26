729aa8d226070faa68ab043b7063ecf224012bd9
Spencer <sp797499@dal.ca>
2025-03-26 01:16:47 -0300

    stars are different sizes and are animated

--- client/src/Background.jsx
+++ client/src/Background.jsx
@@ -4,12 +4,21 @@ import Star from './Star.jsx'
 
 
 const Background = () => {
-    
-    const stars = []
 
-    for (let i = 0; i < 100; i++) {
-        stars.push(i)
-    }
+    const [stars, setStars] = useState([])
+    
+
+    useEffect(() => {
+
+        const tempStars = []
+
+        for (let i = 0; i < 100; i++) {
+            tempStars.push({x: ((Math.random() * 10 - 5) + i * 10) % 100, y: ((Math.random() * 10 - 5) + Math.floor(i / 10) * 10), starType: Math.random()})
+        }
+
+        setStars(tempStars)
+    }, [])
+    
 
 
     return (
@@ -17,7 +26,7 @@ const Background = () => {
             <div  id='background'>
 
                 {stars.map((star, key) => {
-                    return (<Star key={key} index={key}/>)
+                    return (<Star key={key} index={star} x={star.x} y={star.y} starType={star.starType}/>)
                 })}
 
             </div>
--- client/src/Star.jsx
+++ client/src/Star.jsx
@@ -6,9 +6,20 @@
 
 const Star = (props) => {
 
+    const rand = props.starType
+
+    let starType;
+
+    if (rand > 0.90) {
+        starType = "star-lg"
+    } else if (rand > 0.7) {
+        starType = "star-md"
+    } else {
+        starType = "star-sm"
+    }
 
     return (
-        <div className="star" style={{left: `${((Math.random() * 10 - 5) + props.index * 10) % 100}%`, top: `${((Math.random() * 10 - 5) + Math.floor(props.index / 10) * 10)}%`}}>
+        <div className={starType} style={{left: `${props.x}%`, top: `${props.y}%`}}>
         </div>
     )
 }
