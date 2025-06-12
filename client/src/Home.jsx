406b9e8f6ca36dcf67289b2e8fdc802a7e84518f
Spencer <sp797499@dal.ca>
2025-06-11 21:30:15 -0300

    UI for home

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -14,7 +14,11 @@ function Home() {
             <Nav/>
             <Background/>
             <div className="home-container flex justify-center items-center">
-                <Link to="/diviner">diviner</Link>
+                <Link to="/diviner" className="w-1/3 bg-light-color rounded-sm flex justify-center">
+                    <div className="text-white text-5xl px-24 py-8">
+                        diviner
+                    </div>
+                </Link>
             </div>
             <Cursor/>
         </>
