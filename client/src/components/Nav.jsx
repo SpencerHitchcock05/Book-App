d64286ef64ec8608789b0462011109e86c5c2202
Spencer <sp797499@dal.ca>
2025-06-22 23:25:16 -0300

    added knex and user database migration

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -9,11 +9,9 @@ function Nav() {
 
     return (
         <>
-        
             <nav id="nav-bar">
                 <div id="nav-logo"><Link to='/'>Book Search</Link></div>
             </nav>
-        
         </>
     )
 }
