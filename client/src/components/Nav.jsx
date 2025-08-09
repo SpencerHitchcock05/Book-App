79147d59c22bd5d92526013d6c26ba256a98dc79
Spencer <sp797499@dal.ca>
2025-08-08 22:29:39 -0300

    added nav on logout

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom"
-import { useContext } from "react"
-import { UserContext } from "../context/userContext.jsx"
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../context/userContext.jsx";
 import { useAuthHook } from "../hooks/useAuthHook.js";
+import { useNavigate, useLocation } from "react-router-dom";
 
 
 
@@ -10,13 +11,24 @@ import { useAuthHook } from "../hooks/useAuthHook.js";
 function Nav() {
     const { user } = useContext(UserContext);
     const { logout } = useAuthHook();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleLogout = () => {
+        logout()
+        if (location.pathname == '/') {
+            navigate(0)
+        } else {
+            navigate('/')
+        }
+    }
 
     return (
         <>
             <nav id="nav-bar" className="">
                 <div id="nav-logo"><Link to='/'>Book Search</Link></div>
                 <div>{user && user.username}</div>
-                <button onClick={logout}>Logout</button>
+                <button onClick={handleLogout}>Logout</button>
             </nav>
         </>
     )
